583524450c19e3307ed791a2998856ceab9d224f
sbadio <sergebadiojr@gmail.com>
2018-04-22 19:26:49 -0400

    Added Firebase Config

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyADn54F73MbNfiYtDVe9iq9q7gxa0G-JZk",
+    authDomain: "usedbook01.firebaseapp.com",
+    databaseURL: "https://usedbook01.firebaseio.com",
+    projectId: "usedbook01",
+    storageBucket: "usedbook01.appspot.com",
+    messagingSenderId: "589443823730"
+  };
